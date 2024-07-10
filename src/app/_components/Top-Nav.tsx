a7529ad524c42ad3558ea0d64a3109d73b76499d
Rajath U Shetty <rajathushetty56@gmail.com>
2024-07-10 13:48:05 +0530

    Safe commit

--- src/app/_components/Top-Nav.tsx
+++ src/app/_components/Top-Nav.tsx
@@ -1,22 +1,33 @@
 import { Droplets } from "lucide-react";
 import Link from "next/link";
-import { Button, buttonVariants } from "~/components/ui/button";
+import { buttonVariants } from "~/components/ui/button";
+import { getAuthSession } from "~/lib/auth";
 
-export default function TopNav() {
+const TopNav = async () => {
+  const session = await getAuthSession();
+
   return (
     <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b border-gray-500">
       <Link href="/">
         <div className="flex items-center gap-2">
           <Droplets className="h-6 w-6" />
           Aqua Guardians
-        </div></Link>
-      <div className="flex flex-row gap-4">
-        <Link href="/sign-in" className={buttonVariants({ variant: "outline" })}>
-          SIgn In
-        </Link>
+        </div>
+      </Link>
+      <div className="flex flex-row gap-4 mr-6">
+        {session?.user?.id ? (
+          <Link href="/dashboard" className={buttonVariants({ variant: "default" })}>
+           Dashboard
+          </Link>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants({ variant: "outline" })}>
+            Sign In
+          </Link>
+        )}
       </div>
-
     </nav>
+  );
+};
 
-  )
-}
+export default TopNav;
+
